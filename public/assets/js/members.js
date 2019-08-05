5f3f323d822c3d7b3766e65f7ebd424286bca11c
Mark Pascua <markpascua1012@gmail.com>
2019-08-05 14:05:22 -0700

    updated html linked with login

--- public/assets/js/members.js
+++ public/assets/js/members.js
@@ -5,3 +5,14 @@ $(document).ready(function() {
     $(".member-name").text(data.email);
   });
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems, options);
+});
+
+// Or with jQuery
+
+$('.dropdown-trigger').dropdown();
+      
