63978e0ea64031670fc535fffe63a1e57c306be6
Mark Pascua <markpascua1012@gmail.com>
2019-08-05 12:02:02 -0700

    linked login to html

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,41 +1,31 @@
-// *********************************************************************************
-// html-routes.js - this file offers a set of routes for sending users to the various html pages
-// *********************************************************************************
-
-// Dependencies
-// =============================================================
+// Requiring path to so we can use relative routes to our HTML files
 var path = require("path");
 
+// Requiring our custom middleware for checking if a user is logged in
+var isAuthenticated = require("../config/middleware/isAuthenticated");
 
-// Routes
-// =============================================================
 module.exports = function(app) {
 
-  // Each of the below routes just handles the HTML page that the user gets sent to.
+  app.get("/signup", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
 
-  // index route loads view.html
   app.get("/", function(req, res) {
-    res.render(path.join(__dirname, "../public/view.html"));
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
-  // add route loads the add.html page, where users can enter new books to the db
-  app.get("/add", function(req, res) {
-    res.render(path.join(__dirname, "../public/add.html"));
-  });
-
-  // all route loads the all.html page, where all books in the db are displayed
-  app.get("/all", function(req, res) {
-    res.render(path.join(__dirname, "../public/all.html"));
-  });
-
-  // short route loads the short.html page, where short books in the db are displayed
-  app.get("/short", function(req, res) {
-    res.render(path.join(__dirname, "../public/short.html"));
-  });
-
-  // long route loads the long.html page, where long books in the db are displayed
-  app.get("/long", function(req, res) {
-    res.render(path.join(__dirname, "../public/long.html"));
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/members", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/members.html"));
   });
 
 };
