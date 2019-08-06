4e3259b3986bc1e965918715bbab3489687b4850
Tyler Cornwell <tylercornwell@hotmail.com>
2019-08-05 21:07:09 -0700

    working site

---
+++ public/assets/js/script.js
@@ -0,0 +1,9 @@
+//use jquery and ajax
+
+//ajax call to api route, a GET
+
+//in the .then jquery to add list items to the id dropdown1, information from list items will come from database
+
+// data[i].name (how cesna would show on page) use a forward loop to create a list item for everything in the database
+
+//look at GIphy school work
