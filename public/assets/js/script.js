c4dda23654a3f896454cbe56fd8d2d8b6336a8af
Pongnatee Wongthaweesap <Lowmana69@gmail.com>
2019-08-06 20:48:26 -0700

    Commit changes to resolve

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,9 +1,49 @@
-//use jquery and ajax
-
-//ajax call to api route, a GET
-
-//in the .then jquery to add list items to the id dropdown1, information from list items will come from database
-
-// data[i].name (how cesna would show on page) use a forward loop to create a list item for everything in the database
-
-//look at GIphy school work
+$(document).ready(function(){
+    $.ajax({
+        url: "/api/allmakes",
+        type: 'get',
+        dataType: 'json',
+        success:function(response){
+  
+            var len = response.length;
+  
+            $("#dropdown1").empty();
+                
+            $("#dropdown1").append('<option value="" disabled  selected>Choose your option</option>');
+            for( var i = 0; i<len; i++){
+                var id = response[i]['id'];
+                var name = response[i]['name'];
+                
+                $("#dropdown1").append("<option value='"+id+"'>"+name+"</option>");
+  
+            }
+            $('#dropdown1').formSelect();
+        }
+    });
+    $("#dropdown1").change(function(){
+      var makeid = $(this).val();
+      $.ajax({
+          url: "/api/model/make/" + makeid,
+          type: 'get',
+          dataType: 'json',
+          success:function(response){
+    
+              var len = response.length;
+    
+              $("#dropdown2").empty();
+                  
+              $("#dropdown2").append('<option value="" disabled  selected>Choose your option</option>');
+              for( var i = 0; i<len; i++){
+                  var id = response[i]['id'];
+                  var name = response[i]['name'];
+                  
+                  $("#dropdown2").append("<option value='"+id+"'>"+name+"</option>");
+    
+              }
+              $('#dropdown2').formSelect();
+          }
+      });
+  
+    });
+  
+  });
